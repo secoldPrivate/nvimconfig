b45d7ba4e1d922dcc22c4c083dfb2102c0652958
manoj <manoj.clsd.kumar@gmail.com>
2023-08-25 10:19:28 +1000

    init

---
+++ lua/plugins/disable-neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+          "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+          require("nvim-tree").setup {}
+        end,
+    }    
+}
